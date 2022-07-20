2df1e885f660a7053094727b0eef9e3278620a98
ssumthingood <ssumthingood@gmail.com>
2022-07-20 22:30:10 +0900

    0720 webpack practice

---
+++ readme.md
@@ -0,0 +1,4 @@
+vanillaJS 환경에서 webpack을 사용한 프로젝트 빌드
+
+참고
+https://velog.io/@huurray/%EC%9B%B9%ED%8C%A9-Webpack-%EC%9D%98-%EA%B0%9C%EB%85%90%EA%B3%BC-vanilla-js-boilerplate
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./index.css"; // css파일 불러오기
+
+function component() {
+    const element = document.createElement("div");
+
+    element.innerHTML = "Hello World";
+
+    return element;
+}
+
+document.body.appendChild(component());
